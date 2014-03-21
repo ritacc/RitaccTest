1ea27cf01c777a0d999b4646981a3205a4b61ea4
ritacc <ritacc@126.com>
2014-03-21 18:09:45 +0800

    Signed-off-by: ritacc <ritacc@126.com>

---
+++ AnalysisJson/AnalysisJson/BuildHelper.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace AnalysisJson
+{
+	public class BuildHelper
+	{
+		public static void Save(string path, string fileName)
+		{
+			fileName = Regex.Replace(fileName, "[^A-Za-z0-9_.]", "_");
+			//FileStream stream = null;
+			//StreamWriter writer = null;
+			try
+			{
+				//若没有存在指定的目录,则创建之.
+				if (!Directory.Exists(path))
+				{
+					Directory.CreateDirectory(path);
+				}
+
+				//若文件已存在,则删除之.
+				if (File.Exists(path + @"\" + fileName))
+				{
+					File.Delete(path + @"\" + fileName);
+				}
+
+				 
+			}
+			catch (Exception ex)
+			{
+				throw new Exception(ex.Message);
+			}
+			finally
+			{ 
+			}
+		}
+	}
+}
